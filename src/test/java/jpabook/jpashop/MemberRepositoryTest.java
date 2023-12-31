0cbb26854b5f1671f354fb6e6f47389903ab033e
kimjg0424 <jung.gi7777@gmail.com>
2023-12-31 09:22:39 +0900

    first

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpabook.jpashop.domain.Member;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+
+
+
+
+
+}
